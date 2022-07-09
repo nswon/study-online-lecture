14ea3955d6092be105d07050d41fdde47b9f3db4
nswon <swnam0729@gmail.com>
2022-07-09 15:14:49 +0900

    FEAT :: aop 실무사례 1

---
+++ fastcampus/aop/src/main/java/com/example/aop/dto/User.java
@@ -0,0 +1,15 @@
+package com.example.aop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private String id;
+    private String pw;
+    private String email;
+}
